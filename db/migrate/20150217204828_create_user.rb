a113836ff943a8ede70bb48901e15f343adeb850
Sarah McAlear <sarah.mcalear@gmail.com>
2015-02-17 15:52:19 -0500

    new version of app

---
+++ db/migrate/20150217204828_create_user.rb
@@ -0,0 +1,11 @@
+class CreateUser < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null:false, uniqueness: true
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
