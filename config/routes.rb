d934cf86c228227029c7dbc0ff351a1481455986
Sarah McAlear <sarah.mcalear@gmail.com>
2015-02-18 15:57:17 -0500

    adds index for producers to view all his or her cohorts

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
     resources :users
     resources :instructors
-
+    resources :producers
     resources :sessions
 
 
--- db/migrate/20150217204828_create_user.rb
+++ db/migrate/20150217204828_create_user.rb
@@ -3,7 +3,7 @@ class CreateUser < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null:false, uniqueness: true
-      t.string :type
+      t.string :role
       t.string :password_digest
 
       t.timestamps
