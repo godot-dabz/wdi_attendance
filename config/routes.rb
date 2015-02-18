52076e3e8cc6dc676ad588aaea87e418c348aa82
Brian Rucker <rucker.b@gmail.com>
2015-02-17 19:11:36 -0500

    created new routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :users
+    resources :instructors 
 
   # Example resource route with options:
   #   resources :products do
