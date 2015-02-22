dee0a627b0fe082e3869e8daf14e241387a9fb44
Sarah McAlear <sarah.mcalear@gmail.com>
2015-02-22 10:55:43 -0500

    adds chart view in the overview file

--- app/assets/javascripts/chart_test.js
+++ app/assets/javascripts/chart_test.js
@@ -1,35 +1,35 @@
-console.log("main.js linked");
+// console.log("main.js linked");
 
-var data = {
-    labels: ["Cohort 1", "Cohort 2", "Cohort 3", "Cohort 4", "Cohort 5", "Cohort 6", "Cohort 6"],
-    datasets: [
-        {
-            label: "Lateness",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "Excused Absence",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        },
-        {
-            label: "Unexcused Absence",
-            fillColor: "rgba(120,120,120,.9)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [69, 53, 89, 80, 51, 30, 49]
-        },
+// var data = {
+//     labels: ["Cohort 1", "Cohort 2", "Cohort 3", "Cohort 4", "Cohort 5", "Cohort 6", "Cohort 6"],
+//     datasets: [
+//         {
+//             label: "Lateness",
+//             fillColor: "rgba(220,220,220,0.5)",
+//             strokeColor: "rgba(220,220,220,0.8)",
+//             highlightFill: "rgba(220,220,220,0.75)",
+//             highlightStroke: "rgba(220,220,220,1)",
+//             data: [65, 59, 80, 81, 56, 55, 40]
+//         },
+//         {
+//             label: "Excused Absence",
+//             fillColor: "rgba(151,187,205,0.5)",
+//             strokeColor: "rgba(151,187,205,0.8)",
+//             highlightFill: "rgba(151,187,205,0.75)",
+//             highlightStroke: "rgba(151,187,205,1)",
+//             data: [28, 48, 40, 19, 86, 27, 90]
+//         },
+//         {
+//             label: "Unexcused Absence",
+//             fillColor: "rgba(120,120,120,.9)",
+//             strokeColor: "rgba(220,220,220,0.8)",
+//             highlightFill: "rgba(220,220,220,0.75)",
+//             highlightStroke: "rgba(220,220,220,1)",
+//             data: [69, 53, 89, 80, 51, 30, 49]
+//         },
 
-    ]
-};
+//     ]
+// };
 
 
 
