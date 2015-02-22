fc781a5b159fb372ec1d6c12078be8e57980e7ac
Sarah McAlear <sarah.mcalear@gmail.com>
2015-02-22 17:22:31 -0500

    possibly adds ability to receive text messages

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -8,6 +8,30 @@ class TwilioController < ApplicationController
   #   @apiusers = HTTParty.get(url)
   # end
 
+  def index
+    message_body = params["Body"]
+    from_number = params["From"]
+    SMSLogger.log_text_message from_number, message_body
+    binding.pry
+
+  end
+
+  def send_text_message(response)
+    number_to_send_to = "4195089075"
+
+    twilio_sid = TWILIO_KEY_SID
+    twilio_token = 'poop'
+    twilio_phone_number = '5594713142'
+
+    @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @twilio_client.account.sms.messages.create(
+      :from => "+1#{twilio_phone_number}",
+      :to => number_to_send_to,
+      :body => response
+    )
+  end
+
   def response
     if @message.include? "sick"
       response_message = "Feel better. We'll see you tomorrow."
