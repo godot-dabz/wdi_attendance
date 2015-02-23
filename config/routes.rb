c6d46f95dde3b0d00687dad0d5fbbf60b61701c1
Paul Clairmont <pcac25@gmail.com>
2015-02-23 14:47:06 -0500

    removes dead code from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,88 +1,35 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'sessions#new'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :users
+  resources :instructors
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :students do
+    resources :attendances
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  resources :producers do
+    resources :cohorts do
+      get '/overview' => 'cohorts#overview'
+    end
+  end
 
-    resources :users
-    resources :instructors
-
+  resources :cohorts do
+    get '/overview' => 'cohorts#overview'
+    get '/calendar' => 'cohorts#calendar'
     resources :students do
       resources :attendances
     end
+  end
 
-    resources :producers do
-      resources :cohorts do
-        get '/overview' => 'cohorts#overview'
-      end
-    end
+  resources :attendances
 
-    # resources :sessions
-    resources :cohorts do
-      get '/overview' => 'cohorts#overview'
-      get '/calendar' => 'cohorts#calendar'
-      resources :students do
-        resources :attendances
-      end
-    end
-
-    resources :attendances
-
-    # session links
-  get  "/", to: "sessions#new"           # sign in form
+  # session links
+  get  "/", to: "sessions#new"                 # sign in form
   post "/sessions", to: "sessions#create"      # log in
   delete "/sessions", to: "sessions#destroy"   # log out
 
   post "sms", to: "twilio#index"
-  # resources :twilio
 
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
